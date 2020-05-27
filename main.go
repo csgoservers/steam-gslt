3d979c6d9a5d1a1dec89d4cecdd0daade2c898ac
Javi <nhitbh@gmail.com>
2020-05-27 02:49:38 +0200

    Basic implementation to test with command line

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"log"
 
+	"github.com/csgoservers/steam-gameserver-service/pkg/client"
+)
+
+func main() {
+	key := flag.String("key", "", "Steam API key")
+	flag.Parse()
+
+	if *key == "" {
+		log.Println("Steam API key can't be empty")
+		return
+	}
+	steam := client.New(*key)
+	steam.GetAccountList()
 }
